e89dc98cb654416b3e73a1987cf0f8333cadd2dd
ssl4676163 <365912447@qq.com>
2019-01-21 16:54:51 +0800

    2019.1.21

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -3,7 +3,6 @@ import { IonicPageModule } from 'ionic-angular';
 import { MorningComponent } from '../../components/morning/morning';
 import { myReadComponent } from '../../components/myRead/myRead';
 import { GuessYouLikeComponent } from '../../components/guessYouLike/guessYouLike';
-import { VideoComponent } from "../../components/video/video";
 imports: [
 
 ComponentsModule, //添加
@@ -11,6 +10,4 @@ ComponentsModule, //添加
 IonicPageModule.forChild(MorningComponent),
 IonicPageModule.forChild(myReadComponent),
 IonicPageModule.forChild(GuessYouLikeComponent),
-IonicPageModule.forChild(VideoComponent)
-
 ]
